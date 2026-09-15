92648c431aa91a13dbec506174b43ffa1e73373c
Hemanta Das <das.hemant775@gmail.com>
2026-09-15 23:45:33 +0530

    routing implementes - basic routes, route grouping, dynamic routing,layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,31 @@
 import { Link } from "expo-router";
-import "../global.css";
+import "@/global.css";
 import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-red-500">
       <Text className="text-3xl font-bold text-white">NativeWind Works!</Text>
-      <Link href="/onboard" className="mt-4 rounded-md px-4 py-2 bg-green-500"> Go to Onboard</Link>
+      <Link href="/onboard" className="mt-4 rounded-md px-4 py-2 bg-green-500">
+        {" "}
+        Go to Onboard
+      </Link>
+      <Link
+        href="/(tabs)/subscription"
+        className="mt-4 rounded-md px-4 py-2 bg-green-500"
+      >
+        {" "}
+        Go to Subscription
+      </Link>
+      <Link
+        href={{
+          pathname: "/(tabs)/subscriptions/[id]",
+          params: { id: "sporify" },
+        }}
+        className="mt-4 rounded-md px-4 py-2 bg-green-500"
+      >
+        {" "}
+        Subscription spotify
+      </Link>
     </View>
   );
 }
--- app/onboard.tsx
+++ app/onboard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,11 @@ const Onboarding = () => {
   return (
     <View>
       <Text>Onboarding</Text>
+   
+      {/* <Link href="/(auth)/signup" className="mt-4 rounded-md px-4 py-2 bg-green-500">Create Account</Link> */}
+      <Link href="/(auth)/singin" className="mt-4 rounded-md px-4 py-2 bg-green-500"> Sign in</Link>
+    <Link href="/(auth)/signup" className="mt-4 rounded-md px-4 py-2 bg-green-500">Create Account</Link>
+
     </View>
   );
 };
